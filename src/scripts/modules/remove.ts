18fc45604a47bea62f04fb620ccb5949711453b1
Yulia Avramenko <miraclejuliet@yandex.ru>
2024-12-14 00:51:14 +0300

    Finish project

--- src/scripts/modules/remove.ts
+++ src/scripts/modules/remove.ts
@@ -5,3 +5,11 @@ textElements.forEach((element) => {
         element.classList.add('gift__text_hidden');
     });
 });
+
+const textElements1 = document.querySelectorAll('.do__text') as NodeListOf<HTMLElement>;
+
+textElements1.forEach((element) => {
+    element.addEventListener('click', () => {
+        element.classList.add('do__text_hidden');
+    });
+});
